c5e53e1a38d28616256e100300a2df00078ea7c1
Joan Jimenez <hola@joanjimenez.ar>
2024-10-06 18:08:33 -0300

    se saca el observer del core

--- src/main/java/ar/com/quetedebo/core/PayDebts.java
+++ src/main/java/ar/com/quetedebo/core/PayDebts.java
@@ -1,12 +1,10 @@
 package ar.com.quetedebo.core;
 
 import java.util.List;
-import java.util.Observable;
 
-public class PayDebts extends Observable {
+public class PayDebts {
 
 	public void pay(List<Debt> debts) {
-		setChanged();
-        notifyObservers("pay");
+		
 	}
 }
--- src/main/java/ar/com/quetedebo/core/QueTeDebo.java
+++ src/main/java/ar/com/quetedebo/core/QueTeDebo.java
@@ -4,20 +4,19 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
-import java.util.Observer;
 
-public class QueTeDebo implements Observer {
-	private List<Observer> observers = new ArrayList<>();
+public class QueTeDebo extends Observable {
 	private DataLoader dataLoader = new DataLoader();
 	private PayDebts payDebts;
 
 	public QueTeDebo() {
 		this.payDebts = new PayDebts();
-		this.payDebts.addObserver(this);
 	}
 
 	public void pay(List<Debt> debts) {
 		this.payDebts.pay(debts);
+		setChanged();
+        notifyObservers("pay");
 	}
 
 	public List<Debt> getDebts() {
@@ -30,17 +29,4 @@ public class QueTeDebo implements Observer {
 		return debts;
 	}
 	
-	public void subscribe(Observer observer) {
-        observers.add(observer);
-    }
-
-	@Override
-	public void update(Observable arg0, Object arg1) {
-		notifyObservers("pay");
-	}
-	
-	private void notifyObservers(String message) {
-		observers.forEach(observer -> observer.update(payDebts, message));
-    }
-
 }
