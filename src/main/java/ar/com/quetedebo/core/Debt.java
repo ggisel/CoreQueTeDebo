5bb0cc23e04cb45a3d55d8be10ce55068c819d61
Ruminini <63514002+Ruminini@users.noreply.github.com>
2024-10-07 11:59:02 -0300

    Added debt constructors and overriden methods

--- src/main/java/ar/com/quetedebo/core/Debt.java
+++ src/main/java/ar/com/quetedebo/core/Debt.java
@@ -5,6 +5,23 @@ public class Debt {
 	private String addressPayment;
 	private Float amount;
 	private String description;
+	
+	public Debt() {
+    }
+	
+	public Debt(String memberPayment, String addressPayment, Float amount, String description) {
+		this.memberPayment = memberPayment;
+		this.addressPayment = addressPayment;
+		this.amount = amount;
+		this.description = description;
+	}
+
+	public Debt(Debt debt) {
+		memberPayment = debt.memberPayment;
+		addressPayment = debt.addressPayment;
+		amount = debt.amount;
+		description = debt.description;
+	}
 
 	public String getMemberPayment() {
 		return memberPayment;
@@ -38,4 +55,19 @@ public class Debt {
 		this.description = description;
 	}
 
+	@Override
+	public String toString() {
+		return "$" + amount + " to " + memberPayment + " at "+ addressPayment + " for " + description;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj.getClass() != this.getClass()) return false;
+        Debt o = (Debt) obj;
+        return	o.memberPayment.equals(memberPayment) &&
+        		o.addressPayment.equals(addressPayment) &&
+        		o.amount.equals(amount) &&
+        		o.description.equals(description);
+	}
 }
