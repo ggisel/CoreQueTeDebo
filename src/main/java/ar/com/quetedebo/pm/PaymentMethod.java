fd25f0beefd03fc42a588c511bab6085454d8dc3
ggisel <giselle.d.isola@gmail.com>
2024-10-07 06:52:33 -0300

    cambios en el payment method

--- src/main/java/ar/com/quetedebo/pm/PaymentMethod.java
+++ src/main/java/ar/com/quetedebo/pm/PaymentMethod.java
@@ -1,5 +1,7 @@
 package ar.com.quetedebo.pm;
+import ar.com.quetedebo.core.Debt;
 
-public class PaymentMethod {
-
+public interface PaymentMethod {
+    String getName();
+    void processPayment(Debt debt);
 }
